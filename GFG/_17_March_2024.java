9e0b064b3bbba0227420fb1d5c390f5a483e7909
Animesh Basak <basakanimesh16@gmail.com>
2024-03-17 04:04:29 +0530

    POTD

---
+++ GFG/_17_March_2024.java
@@ -0,0 +1,44 @@
+package POTD.GFG;
+
+public class _17_March_2024 {
+    /*
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+    class Solution {
+
+        public static int countPairs(
+                LinkedList<Integer> head1, LinkedList<Integer> head2, int x) {
+
+            HashSet<Integer> set = new HashSet<>();
+            int count = 0;
+
+            // Traverse head1 and store elements in the set
+            for (Integer num : head1) {
+                set.add(num);
+            }
+
+            // Traverse head2 and check for complement in the set
+            for (Integer num : head2) {
+                int complement = x - num;
+                if (set.contains(complement)) {
+                    count++;
+                }
+            }
+
+            return count;
+
+        }
+    }
+
+}

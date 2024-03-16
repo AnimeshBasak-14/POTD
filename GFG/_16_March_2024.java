06c7c7359ac4f28bf4e3ab756a82863066048097
Animesh Basak <basakanimesh16@gmail.com>
2024-03-16 17:50:38 +0530

    POTD

--- GFG/_16_March_2024.java
+++ GFG/_16_March_2024.java
@@ -1,20 +1,25 @@
 package POTD.GFG;
 
 public class _16_March_2024 {
-    void deleteNode(Node del_node)
+    class Solution
     {
-        if(del_node == null) // If linked list is empty
-            return;
-        else{
-            if(del_node.next == null){
-                // System.out.println("This is last node, require head, can't be freed\n");
+        void deleteNode(Node del_node)
+        {
+            if(del_node == null) // If linked list is empty
                 return;
+            else{
+                if(del_node.next == null){
+                    // System.out.println("This is last node, require head, can't be freed\n");
+                    return;
+                }
             }
-        }
-        // copy data of the next node to current node
-        del_node.data = del_node.next.data;
+            // copy data of the next node to current node
+            del_node.data = del_node.next.data;
 
-        // perform conventional deletion
-        del_node.next = del_node.next.next;
+            // perform conventional deletion
+            del_node.next = del_node.next.next;
+        }
+
     }
 }
+
---
+++ leetcode/_16_March_2024.java
@@ -0,0 +1,27 @@
+package POTD.leetcode;
+
+public class _16_March_2024 {
+
+    class Solution {
+        public int findMaxLength(int[] nums) {
+            int N = nums.length;
+            int[] mp = new int[2*N+2];
+            int current = N;
+            int result = 0;
+
+            for(int i = 0; i < N; i++) {
+                current += (nums[i] << 1) - 1;
+                if(current == N) {
+                    result = i+1;
+                }
+                else if(mp[current] == 0) {
+                    mp[current] = i+1;
+                }
+                else {
+                    result = Math.max(result, i - mp[current]+1);
+                }
+            }
+            return result;
+        }
+    }
+}

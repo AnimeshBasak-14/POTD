d170c4c72c3678665a5fc9a7f2d505f76f05cf27
Animesh Basak <basakanimesh16@gmail.com>
2024-03-16 15:05:24 +0530

    POTD

---
+++ GFG/_16_March_2024.java
@@ -0,0 +1,20 @@
+package POTD.GFG;
+
+public class _16_March_2024 {
+    void deleteNode(Node del_node)
+    {
+        if(del_node == null) // If linked list is empty
+            return;
+        else{
+            if(del_node.next == null){
+                // System.out.println("This is last node, require head, can't be freed\n");
+                return;
+            }
+        }
+        // copy data of the next node to current node
+        del_node.data = del_node.next.data;
+
+        // perform conventional deletion
+        del_node.next = del_node.next.next;
+    }
+}

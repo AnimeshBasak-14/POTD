17fbb1fd7982e365f2f8f04d8d82d6070612cce1
Animesh Basak <basakanimesh16@gmail.com>
2024-04-30 01:59:59 +0530

    POTD

---
+++ GFG/_2024_04_29.java
@@ -0,0 +1,38 @@
+package POTD.GFG;
+
+public class _2024_04_29 {
+    /* Link list Node */
+class Node
+{
+	Node next;
+	int data;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+    class Solution
+    {
+        /*You are required to complete this method*/
+        Node delete(Node head, int k)
+        {
+            // Your code here
+            if (k == 1) return null;
+            int ct = 1;
+            Node node = head;
+            while (node != null) {
+                if (ct == k-1) {
+                    if (node.next == null) break;
+                    node.next = node.next.next;
+                    // node = node.next;
+                    ct = 1;
+                } else {
+                    ct++;
+                }
+                node = node.next;
+            }
+            return head;
+        }
+    }
+}
